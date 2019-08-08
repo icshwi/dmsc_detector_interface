50631a58c5aefc97b8abcb079fbbee71a09df525
Will Smith <william.smith@stfc.ac.uk>
2019-08-08 18:57:25 +0200

    fixed sync and sync check. nsecs are read back as nsecs not ticks. needed conversion

--- dmsc_detector_interfaceApp/src/getTimeStamp_aSub.c
+++ dmsc_detector_interfaceApp/src/getTimeStamp_aSub.c
@@ -18,18 +18,36 @@ unsigned long tickDelta=0; // given in ticks
 unsigned long firmware_ticks = 0;
 unsigned long firmware_seconds = 0;
 unsigned long ICSTICKSPERSECOND = 88052500;
-
-	
+unsigned long NANOSPERSECOND = 1000000000;
+double NANOSPERTICK;
+double ticksD;
+unsigned long ticksC; 
 	if (!dbGetTimeStamp(&prec->inpa, &tick)){ 			
 				
+		printf("Initial sec %lu ICS nsec %lu ",tick.secPastEpoch,tick.nsec);
+		
+		NANOSPERTICK = ((double)NANOSPERSECOND/(double)ICSTICKSPERSECOND);
+		printf("NANOSPERTICK = %f ",NANOSPERTICK);
 		
 		tickDelta = *(long *)prec->b; //in ticks
+		printf("tickDelta = %lu ",tickDelta);
+				
+		//convert the tick.nsec from nsecs to ticks
+		ticksD = tick.nsec/NANOSPERTICK;
+		printf("ticksD = %f ",ticksD);
 		
+		ticksC = floor(ticksD);
+		printf("ticksC = %lu ",ticksC);
 		
 		//Deal with the carry
-		nSecRem = (tick.nsec + tickDelta)%ICSTICKSPERSECOND; //implies that the nsec field of the tick struct is in fractional nanos!
-		carrySec = floor((tick.nsec + tickDelta)/ICSTICKSPERSECOND);
-	
+		nSecRem = (ticksC + tickDelta)%ICSTICKSPERSECOND; 
+		printf("ticksC + ticksDelta = %lu ",ticksC + tickDelta);
+		printf("nSecRem = %lu ",nSecRem);
+		
+		
+		carrySec = floor((ticksC + tickDelta)/ICSTICKSPERSECOND);
+	 	printf("CarrySec = %lu\n",carrySec); 
+		
 		//convert ICS ticks into FPGA ticks
 		firmware_ticks = floor((nSecRem/2)); //to account for the prescaler clock output
 		firmware_seconds = tick.secPastEpoch + carrySec + 631152000; //Add the time to shift to UNIX time
